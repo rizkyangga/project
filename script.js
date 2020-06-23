3c91358bdaf7575ed34d163381ff62ea39f1ea8a
Rizky <67300386+rizkyangga@users.noreply.github.com>
2020-06-23 09:09:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Selamat datang di Tim I47");
+const firstName = prompt("Siapa nama depanmu?");
+
+const user = {
+   name: {
+       first: firstName,
+   },
+};
+
+if (user.kabar === "baik") {
+   alert("Semoga hari mu menyenangkan " + user.name.first + "!");
+} else {
+alert("Semoga hari mu menyenangkan " + user.name.first + "!");
+}
